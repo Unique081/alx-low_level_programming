601e9820413e618fcc74056f8c99ca97059a56b7
Unique081 <gracenkirukanmd@gmail.com>
2023-08-12 06:28:19 -0700

    prints all aphabets in a specified case with exemption

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,30 @@
-int main() {
-	    char letter = 'a';
+#include <stdio.h>
 
-	        while (letter <= 'z') {
-			        if (letter != 'e' && letter != 'q') {
-					            putchar(letter);
-						            }
-				        letter++;
-					    }
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Description: Prints the lowercase alphabet excluding 'q' and 'e',
+ *     *              followed by a new line, using the putchar function.
+ *      *
+ *       * Return: Always 0 (Success)
+ *        */
+int main(void)
+{
+		char letter;
 
-		    putchar('\n');
+			/* Loop through the alphabet */
+			for (letter = 'a'; letter <= 'z'; letter++)
+					{
+								/* Check if the letter is not 'q' or 'e' */
+								if (letter != 'q' && letter != 'e')
+											{
+															/* Print the current letter */
+															putchar(letter);
+																	}
+									}
 
-		        return 0;
+				/* Print a new line */
+				putchar('\n');
+
+					return (0);
 }
-
