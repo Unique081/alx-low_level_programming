14886bd8cde5355a2a654be64009b4606515d714
Unique081 <gracenkirukanmd@gmail.com>
2023-08-11 17:02:12 -0700

    more alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+int main(void)
+{
+	    char lowercase = 'a';
+	        char uppercase = 'A';
+		    
+		    while (lowercase <= 'z' && uppercase <= 'Z')
+			        {
+					        putchar(lowercase);
+						        lowercase++;
+							        
+							        putchar(uppercase);
+								        uppercase++;
+									    }
+		        
+		        putchar('\n');
+			    
+			    return 0;
+}
+
