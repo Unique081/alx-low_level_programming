0c46c586b16324f25ad5a7cd34e5b188f7506d3f
Unique081 <gracenkirukanmd@gmail.com>
2023-08-13 07:18:39 -0700

    wahala

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point of the program
- *
- * Return: Alway 0 (Success)
+ * main - Entry point
+ * Description: Prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i:
-		for (i = 0; i < 10; i++)
-
-		{
-			printf("%d", i);
-		}
-	printf("\n");
-	return (0);
+	int digit = 0;
+	while (digit < 10)
+		char c = digit + '0';
+	write(1, &c, 1);
+	digit++;
+}
+write(1, "\n", 1);
+return (0);
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Description: Prints all single digit numbers of base 10, followed by a new line.
- *     * Restrictions: Only use putchar function and call it only twice.
- *      *
- *       * Return: Always 0 (Success)
- *        */
+ * main - Entry point
+ *
+ * Description: Prints all single digit nums of base 1o, + new line.
+ * Restrictions: Only use putchar function and call it only twice.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int i;
+	int i;
+	for (i = 0; i < 10; i++)
 
-			for (i = 0; i < 10; i++)
-					{
-								putchar(i + '0');
-									}
-
-				putchar('\n');
-
-					return (0);
+		
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
 }
